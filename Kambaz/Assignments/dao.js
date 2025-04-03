3bdb2b8b24339ff75d6bf3371e9bfaab71a88f59
Nikhil Gupta <99nikhilgupta@gmail.com>
2025-04-02 23:42:19 -0400

    Assignment 5

---
+++ Kambaz/Assignments/dao.js
@@ -0,0 +1,32 @@
+import Database from "../Database/index.js";
+
+export function getAssignmentsForCourse(courseId) {
+  return Database.assignments.filter((assignment) => assignment.course === courseId);
+}
+
+export function getAssignmentById(assignmentId) {
+  return Database.assignments.find((assignment) => assignment._id === assignmentId);
+}
+
+export function createAssignment(assignment) {
+  const newAssignment = { ...assignment };
+  Database.assignments = [...Database.assignments, newAssignment];
+  return newAssignment;
+}
+
+export function deleteAssignment(assignmentId) {
+  Database.assignments = Database.assignments.filter((assignment) => assignment._id !== assignmentId);
+}
+
+export function updateAssignment(assignmentId, assignmentUpdates) {
+  const assignment = Database.assignments.find((assignment) => assignment._id === assignmentId);
+  if (!assignment) {
+    return null;
+  }
+  Object.assign(assignment, assignmentUpdates);
+  return assignment;
+}
+
+export function getAllAssignments() {
+  return Database.assignments;
+}
